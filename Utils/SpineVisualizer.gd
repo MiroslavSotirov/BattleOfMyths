0cf60c3c7467f803cc84364f5e577f0e9f634e59
Stefano <stefano@elysiumstudios.se>
2022-06-03 11:12:40 +0300

    Debug tool

---
+++ Utils/SpineVisualizer.gd
@@ -0,0 +1,11 @@
+tool
+extends SpineSpriteExtension
+class_name SpineDebugVisualizer
+func _get_tool_buttons(): return ["play_current_anim"]
+
+func _ready():
+	if(Engine.editor_hint): return;
+	._ready();
+
+func play_current_anim():
+	._ready();
