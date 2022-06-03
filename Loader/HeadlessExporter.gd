23bc2f4aabb5bd1509c63159aba9dd6b89a2b26c
Stefano <stefano@elysiumstudios.se>
2022-06-03 13:00:01 +0300

    Build process

--- Loader/HeadlessExporter.gd
+++ Loader/HeadlessExporter.gd
@@ -15,7 +15,7 @@ func export_all_pck(quit=false):
 	print("EXPORTING PCK...")
 	var children = get_children();
 	for child in children:
-		child.export_pck();
+		child.export_pck(true);
 		#yield(child, "export_completed")
 	
 	print("COMPLETED")
