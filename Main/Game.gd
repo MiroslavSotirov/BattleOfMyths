a15bf28ab0d570dc05f49b9e6d6a828eabe4f5e3
Stefano <stefano@elysiumstudios.se>
2022-06-06 13:02:29 +0300

    Added new state and preparation for fs

--- Main/Game.gd
+++ Main/Game.gd
@@ -3,3 +3,15 @@ class_name Game
 
 func _ready():
 	Globals.register_singleton("Game", self);
+
+func switch_to_dragon_mode():
+	$SlotContainer/Background/AnimationPlayer.play("to_dragon")
+	$SlotContainer/Slot/Overlap/AnimationPlayer.play("to_dragon")
+	
+func switch_to_tiger_mode():
+	$SlotContainer/Background/AnimationPlayer.play("to_tiger")
+	$SlotContainer/Slot/Overlap/AnimationPlayer.play("to_tiger")
+
+func switch_to_normal():
+	$SlotContainer/Background/AnimationPlayer.play("to_normal")
+	$SlotContainer/Slot/Overlap/AnimationPlayer.play("to_normal")
