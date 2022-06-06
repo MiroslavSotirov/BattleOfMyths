e1c9fb40c54b4b15be1f129ca46dd8df2a463057
Stefano <stefano@elysiumstudios.se>
2022-06-06 17:31:19 +0300

    Scenario update

--- Main/Game.gd
+++ Main/Game.gd
@@ -1,17 +1,21 @@
 extends Node
 class_name Game
+var current_state : String = "normal";
 
 func _ready():
 	Globals.register_singleton("Game", self);
 
 func switch_to_dragon_mode():
+	current_state = "dragon";
 	$SlotContainer/Background/AnimationPlayer.play("to_dragon")
 	$SlotContainer/Slot/Overlap/AnimationPlayer.play("to_dragon")
 	
 func switch_to_tiger_mode():
+	current_state = "tiger"
 	$SlotContainer/Background/AnimationPlayer.play("to_tiger")
 	$SlotContainer/Slot/Overlap/AnimationPlayer.play("to_tiger")
 
 func switch_to_normal():
+	current_state = "normal"
 	$SlotContainer/Background/AnimationPlayer.play("to_normal")
 	$SlotContainer/Slot/Overlap/AnimationPlayer.play("to_normal")
