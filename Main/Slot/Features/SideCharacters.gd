c98f2d341094f2f8a3bc12bc8009806206167b61
Stefano <stefano@elysiumstudios.se>
2022-06-21 15:50:06 +0300

    Review characters

--- Main/Slot/Features/SideCharacters.gd
+++ Main/Slot/Features/SideCharacters.gd
@@ -2,10 +2,15 @@ extends Node
 
 func _ready():
 	Globals.register_singleton("SideCharacters", self);
+	get_node("Tiger").play_anim("idle", true, 0.5);
+	get_node("Dragon").play_anim("idle", true, 0.5);
 	
 func play(anim, delay=0.0):
 	if(delay > 0.0): yield(get_tree().create_timer(delay), "timeout");
 	$AnimationPlayer.play(anim);
 
 func spine_play(character, anim):
-	get_node(character).play_anim_then_loop(anim, "idle");
+	var chr = get_node(character);
+	chr.play_anim(anim, false);
+	yield(chr, "animation_complete");
+	chr.play_anim("idle", true, 0.5);
