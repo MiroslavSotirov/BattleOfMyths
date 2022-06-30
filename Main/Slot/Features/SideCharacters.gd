71d7f9c44efcf44ae9803e497cf604acf9bf05bb
Stefano <stefano@elysiumstudios.se>
2022-06-30 11:55:37 +0300

    Heroes hide

--- Main/Slot/Features/SideCharacters.gd
+++ Main/Slot/Features/SideCharacters.gd
@@ -4,6 +4,7 @@ func _ready():
 	Globals.register_singleton("SideCharacters", self);
 	get_node("Tiger").play_anim("idle", true, 0.5);
 	get_node("Dragon").play_anim("idle", true, 0.5);
+	Globals.connect("resolutionchanged", self, "on_resolution_changed");
 	
 func play(anim, delay=0.0):
 	if(delay > 0.0): yield(get_tree().create_timer(delay), "timeout");
@@ -14,3 +15,12 @@ func spine_play(character, anim):
 	chr.play_anim(anim, false);
 	yield(chr, "animation_complete");
 	chr.play_anim("idle", true, 0.5);
+
+func on_resolution_changed(landscape, portrait, screenratio, zoom):
+	if(screenratio < 0.5):
+		if($AnimationPlayerRoot.assigned_animation != "Show"):
+			$AnimationPlayerRoot.play("Show");
+	else:
+		if($AnimationPlayerRoot.assigned_animation != "Hide"):
+			$AnimationPlayerRoot.play("Hide");
+		
